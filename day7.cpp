4a30246f7fd0e971a1616aab0271ae3621b97b4b
xiazi97 <44505023+xiazi97@users.noreply.github.com>
2019-05-27 23:05:44 +0800

    交错01串

---
+++ day7.cpp
@@ -0,0 +1,27 @@
+/*
+如果一个01串任意两个相邻位置的字符都是不一样的,我们就叫这个01串为交错01串。例如: "1","10101","0101010"都是交错01串。
+小易现在有一个01串s,小易想找出一个最长的连续子串,并且这个子串是一个交错01串。小易需要你帮帮忙求出最长的这样的子串的长度是多少。
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    int len = 1;
+    int res = 0;
+    for(int i=1;i<str.size();++i)
+    {
+        if(str[i-1] != str[i])
+        {
+            len++;
+            res = max(len,res);
+        }
+        else
+            len = 1;
+    }
+    cout << res << endl;
+    return 0;
+}
