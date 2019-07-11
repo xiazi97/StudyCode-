5ef5ea96275a9863d156a215a8c7ff0f626e7e12
xiazi97 <44505023+xiazi97@users.noreply.github.com>
2019-07-11 23:18:47 +0800

    Update day13.cpp

--- day13.cpp
+++ day13.cpp
@@ -1,14 +1,14 @@
 /*
-���ӣ�https://www.nowcoder.com/questionTerminal/3a3577b9d3294fb7845b96a9cd2e099c
-��Դ��ţ����
+链接：https://www.nowcoder.com/questionTerminal/3a3577b9d3294fb7845b96a9cd2e099c
+来源：牛客网
 
-СQ���ڸ�һ������Ϊn�ĵ�·���·�ư��÷�����
+小Q正在给一条长度为n的道路设计路灯安置方案。
 
-Ϊ�����������,СQ�ѵ�·��Ϊn������,��Ҫ�����ĵط���'.'��ʾ, ����Ҫ�������ϰ��������'X'��ʾ��
+为了让问题更简单,小Q把道路视为n个方格,需要照亮的地方用'.'表示, 不需要照亮的障碍物格子用'X'表示。
 
-СQ����Ҫ�ڵ�·������һЩ·��, ���ڰ�����posλ�õ�·��, ��յ·�ƿ�������pos - 1, pos, pos + 1������λ�á�
+小Q现在要在道路上设置一些路灯, 对于安置在pos位置的路灯, 这盏路灯可以照亮pos - 1, pos, pos + 1这三个位置。
 
-СQϣ���ܰ��þ����ٵ�·����������'.'����, ϣ�����ܰ�������һ��������Ҫ����յ·�ơ�
+小Q希望能安置尽量少的路灯照亮所有'.'区域, 希望你能帮他计算一下最少需要多少盏路灯。
 */
 
 #include <iostream>
@@ -27,7 +27,7 @@ int main()
 		int res = 0;
 		for (int i = 0; i<num;) {
 			if (str[i] == '.') {
-				i += 3;
+				i += 3;		//当前.附近的三个都可照亮
 				res++;
 			}
 			else
