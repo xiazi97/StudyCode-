8a220967b73cc3c62e921791fd9e245ecf3b8946
xiazi97 <44505023+xiazi97@users.noreply.github.com>
2019-09-02 22:43:17 +0800

    判断二叉树是否平衡

---
+++ day24.cpp
@@ -0,0 +1,36 @@
+/*
+ÅÐ¶Ï¶þ²æÊ÷ÊÇ·ñÆ½ºâ
+*/
+#include <iostream>
+#include <algorithm>
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+		val(x), left(nullptr), right(nullptr) {
+	}
+};
+
+class Balance {
+public:
+	int Height(TreeNode* proot)
+	{
+		if (nullptr == proot)
+			return 0;
+
+		int left = Height(proot->left);
+		int right = Height(proot->right);
+		return left > right ? left + 1 : right + 1;
+	}
+
+	bool isBalance(TreeNode* root) {
+		if (nullptr == root)
+			return true;
+		int left = Height(root->left);
+		int right = Height(root->right);
+		if (abs(left - right) > 1)
+			return false;
+		return isBalance(root->left) && isBalance(root->right);
+	}
+};
